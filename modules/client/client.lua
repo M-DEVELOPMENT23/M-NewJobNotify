f8ec261d88439884511f2bec8160d66ab97435e5
M-DEVELOPMENT <69606982+M-DEVELOPMENT23@users.noreply.github.com>
2024-02-19 19:04:35 +0100

    Update client.lua

--- modules/client/client.lua
+++ modules/client/client.lua
@@ -1,20 +1,18 @@
-local displayed = true
+local notification = true
+local displayed = false
 
 function SendNUIMessageWithOptions(type, job, text, isClosing)
-    if displayed then
-        local displayed = false
+    if not displayed then
         local jobConfig = Config.Jobs[job]
-
+        displayed = true
         if Config.Debug then
-            print("[M-JobNotifycatiosnV2] [DEBUG] " .. jobConfig.name)
-            print("[M-JobNotifycatiosnV2] [DEBUG] " .. jobConfig.image)
-            print("[M-JobNotifycatiosnV2] [DEBUG] " .. jobConfig.titlecolor)
-            print("[M-JobNotifycatiosnV2] [DEBUG] " .. text)
-            print("[M-JobNotifycatiosnV2] [DEBUG] " .. jobConfig.textcolor)
-            print("[M-JobNotifycatiosnV2] [DEBUG] " .. jobConfig.icon)
+            print("[M-JobNotificationsV2] [DEBUG] " .. jobConfig.name)
+            print("[M-JobNotificationsV2] [DEBUG] " .. jobConfig.image)
+            print("[M-JobNotificationsV2] [DEBUG] " .. jobConfig.titlecolor)
+            print("[M-JobNotificationsV2] [DEBUG] " .. text)
+            print("[M-JobNotificationsV2] [DEBUG] " .. jobConfig.textcolor)
+            print("[M-JobNotificationsV2] [DEBUG] " .. jobConfig.icon)
         end
-
-
         SendNUIMessage({
             type = type,
             jobname = jobConfig.name,
@@ -24,43 +22,92 @@ function SendNUIMessageWithOptions(type, job, text, isClosing)
             textcolor = jobConfig.textcolor,
             icon = jobConfig.icon,
             footertext = Config.FooterText,
-
             backgroundcolor = Config.Ui.backgroundcolor,
             boxshadow = Config.Ui.boxshadow,
             borderradius = Config.Ui.borderradius,
             opacity = Config.Ui.opacity,
             top = Config.Ui.top,
             left = Config.Ui.left,
-
             borderradiusimg = Config.Ui.borderradiusimg,
             titlefontsize = Config.Ui.fontsize,
             titlefontweight = Config.Ui.fontweight,
             titlefilter = Config.Ui.filter,
         })
 
+        local mostrado = false
+
+        Citizen.CreateThread(function()
+            while not mostrado do
+                Citizen.Wait(0)
+                if not displayed then
+                    if IsControlJustPressed(0, 38) then
+                        local coords = jobConfig.coords
+                        if Config.Debug then
+                            print("[M-JobNotificationsV2] [DEBUG] coordenadas marcadas: " .. coords.x .. " " .. coords.y)
+                        end
+
+                        SetNewWaypoint(coords.x, coords.y)
+
+                        mostrado = true
+                    end
+                end
+            end
+        end)
+
+        Wait(2500)
+
+        displayed = false
+    end
+end
+
+function ManageBusinessState(job, text, isClosing)
+    SendNUIMessageWithOptions('abrirnegocio', job, text, isClosing)
+end
+
+function OpenBusiness(job)
+    ManageBusinessState(job, Config.Jobs[job].openbusinesstext)
+end
+
+function CloseBusiness(job)
+    ManageBusinessState(job, Config.Jobs[job].closebusinesstext, true)
+end
+
+function CustomTextBusiness(text, job)
+    SendNUIMessageWithOptions('customtext', job, text)
+end
+
+function ManageNotification(title, text, imageurl, icon, jobcoords, TextFooter)
+    if notification then
+        local notification = false
+        SendNUIMessage({
+            type = "notification",
+            jobname = title,
+            image = imageurl,
+            texto = text,
+            icon = icon,
+            footertext = TextFooter,
+        })
+
+
         Citizen.CreateThread(function()
             while true do
                 Citizen.Wait(0)
-                if not displayed and IsControlJustPressed(0, 38) then -- 38 es el valor de la tecla 'E'
-                    local coords = jobConfig.coords
+                if not notification and IsControlJustPressed(0, 38) then
+                    local coords = jobcoords
+
                     if Config.Debug then
                         print("[M-JobNotifycatiosnV2] [DEBUG] coordenadas marcadas: " .. coords.x .. " " .. coords.y)
                     end
 
                     SetNewWaypoint(coords.x, coords.y)
 
-                    local notificationText = "Coordenadas marcadas hacia: " .. jobConfig.name
-                    if isClosing then
-                        notificationText = "Negocio cerrado: " .. jobConfig.name
-                    end
-
                     if Config.notificationScript == "QBCore" then
                         local QBCore = exports['qb-core']:GetCoreObject()
-                        QBCore.Functions.Notify(notificationText)
+                        QBCore.Functions.Notify("business marked on gps")
                     elseif Config.notificationScript == "ox" then
                         lib.notify({
                             title = 'GPS',
-                            description = notificationText,
+                            description = "Business marked on gps",
                             type = 'inform'
                         })
                     end
@@ -70,20 +117,7 @@ function SendNUIMessageWithOptions(type, job, text, isClosing)
             end
         end)
     end
-
-    displayed = true
-end
-
-function OpenBusiness(job)
-    SendNUIMessageWithOptions('abrirnegocio', job, Config.Jobs[job].openbusinesstext)
-end
-
-function CloseBusiness(job)
-    SendNUIMessageWithOptions('abrirnegocio', job, Config.Jobs[job].closebusinesstext, true)
-end
-
-function CustomTextBusiness(text, job)
-    SendNUIMessageWithOptions('customtext', job, text)
+    notification = true
 end
 
 RegisterNetEvent('m:jobnotifyv2:abrirbusiness')
@@ -95,5 +129,12 @@ AddEventHandler('m:jobnotifyv2:closebusiness', CloseBusiness)
 RegisterNetEvent('m:jobnotifyv2:customtextbusiness')
 AddEventHandler('m:jobnotifyv2:customtextbusiness', CustomTextBusiness)
 
+RegisterNetEvent('m:jobnotifyv2:opennotification')
+AddEventHandler('m:jobnotifyv2:opennotification', ManageNotification)
 
-
+RegisterNetEvent('m:jobnotifyv2:closenotification')
+AddEventHandler('m:jobnotifyv2:closenotification', function()
+    SendNUIMessage({
+        type = "closenotification",
+    })
+end)
